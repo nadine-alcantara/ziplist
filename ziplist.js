eeafcc206ada95693ca0d5795a1c61d9aa8c34ae
Nadine Alcantara <nadine-alcantara@users.noreply.github.com>
2019-09-22 08:00:00 -1000

    Finished Exercise.

---
+++ ziplist.js
@@ -0,0 +1,16 @@
+const list1 = ['a', 'b', 'c'];
+const list2 = [1, 2, 3];
+
+function zipList(alpha, nums) {
+  const temp = [];
+  for (let i = 0; i < alpha.length; i++) {
+    temp.push(alpha[i], nums[i]);
+  }
+  return temp;
+}
+console.log(zipList(list1, list2));
+
+function zipListTheSimpleWay(alpha, nums){
+  return _.flatten(_.zip(alpha, nums));
+}
+console.log(zipListTheSimpleWay(list1, list2));
